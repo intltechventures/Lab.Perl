0896933e8b20ca4f1019ad823ead41c298f6ce9b
Kelvin D. Meeks <kmeeks@intltechventures.com>
2016-05-20 16:56:24 -0700

    Created test.email.pl

---
+++ Examples/regex/test.email.pl
@@ -0,0 +1,7 @@
+#! usr/bin/perl
+# a pattern for testing email addresses...
+# /^[a-z0-9!#$%&'*+\/=?^_`{|}~.-]+@[a-z0-9]([a-z0-9-]*[a-z0-9])?(\.[a-z0-9]([a-z0-9-]*[a-z0-9])?)*$/
+#
+# from:
+# https://github.com/angular/angular.js/blob/fa79eaa816aa27c6d1b3c084b8372f9c17c8d5a3/src/ng/directive/input.js#L27
+#
